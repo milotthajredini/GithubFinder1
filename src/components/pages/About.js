9b26ed4af948d73422dd8aec7ebe629735c3ee66
milotthajredini <milothajredini@gmail.com>
2020-11-22 16:25:00 +0100

    final commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About This App</h1>
+      <p>Lorem Ipsum</p>
+    </Fragment>
+  );
+};
+
+export default About;
